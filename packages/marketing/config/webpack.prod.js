ae7706726e11e85db930179d05a0751a4c5ff39b
Muhammad Rajput <muhammadrajput99@gmail.com>
2022-02-21 18:03:02 -0500

    added webpack for marketing

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,22 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const {merge} = require('webpack-merge')
+const commonConfig = require("./webpack.common")
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[hashcontent].js'
+  },
+  plugins:[
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes:{
+        "./marketingApp": "./src/bootstrap.js"
+      },
+      shared: { 'react': { singleton: true }, 'react-dom': { singleton: true } }
+    })
+  ]
+}
+
+module.exports = merge(commonConfig, prodConfig);
