1883129ca34c5fc1e15cf8b6603b8a9c41ff2238
Muhammad Rajput <muhammadrajput99@gmail.com>
2022-02-21 18:14:00 -0500

    m

--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -161,7 +161,7 @@ export default function Pricing() {
           color="textSecondary"
           component="p"
         >
-          Quickly builds an effective pricing table for your potential customers
+          Quick builds an effective pricing table for your potential customers
           with this layout. It&apos;s built with default Material-UI components
           with little customization.
         </Typography>
