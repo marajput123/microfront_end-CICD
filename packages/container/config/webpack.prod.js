0ec9b61bd9fc182099f3293c549211c89c17e296
Muhammad Rajput <muhammadrajput99@gmail.com>
2022-02-21 16:27:40 -0500

    fixed js publicPath

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,7 +8,8 @@ const CDN_DOMAIN = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: "/container/"
   },
   plugins: [
     new ModuleFederationPlugin({
