1e9a07777c75e898d86e89f4185e35c1e7fb85a7
Muhammad Rajput <muhammadrajput99@gmail.com>
2022-02-21 18:10:50 -0500

    m

--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -161,7 +161,7 @@ export default function Pricing() {
           color="textSecondary"
           component="p"
         >
-          Quickl builds an effective pricing table for your potential customers
+          Quicky builds an effective pricing table for your potential customers
           with this layout. It&apos;s built with default Material-UI components
           with little customization.
         </Typography>
