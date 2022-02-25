eb6e882b97d9eb48e21968a8ab9b538c0777b1c2
Muhammad Rajput <muhammadrajput99@gmail.com>
2022-02-25 12:12:10 -0500

    Updated s3 bucket path

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Page
+              Homes Page
             </Typography>
             <Typography
               variant="h5"
